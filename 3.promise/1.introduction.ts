a15a292972df4f0b537c14067754b32d0c3ad3ee
shiyisen <shiyisen@baidu.com>
2020-12-21 22:15:27 +0800

    promise1

--- 3.promise/1.introduction.ts
+++ 3.promise/1.introduction.ts
@@ -9,85 +9,106 @@ import {
 } from './2.implement';
 
 import fs from 'fs';
-import path from 'path';
+import path, { resolve } from 'path';
 
 const _path = (p: string): string => {
     return path.resolve(__dirname, p);
 
 };
 
-module promise31_sync {
-    console.log('---------- sync ----------')
-    const p = new Promise_Sync<string>((resolve, reject) => {
-        /* 1 */
-        // resolve('玩具少');
-        // reject('玩具多');
+// module promise31_sync {
+//     const p = new Promise_Sync<string>((resolve, reject) => {
+//         /* 1 */
+//         // resolve('玩具少');
+//         // reject('玩具多');
 
-        /* 2 */
-        // throw new Error('报错');
-    });
-    p.then(val => {
-        console.log(val, 'success');
-        return val;
-    }, err => {
-        console.log(err, 'fail');
-    });
-    p.then(val => {
-        console.log(val, 'success');
-    }, err => {
-        console.log(err, 'fail');
-    });
-}
+//         /* 2 */
+//         // throw new Error('报错');
+//     });
+//     p.then(val => {
+//         console.log(val, 'success');
+//         return val;
+//     }, err => {
+//         console.log(err, 'fail');
+//     });
+//     p.then(val => {
+//         console.log(val, 'success');
+//     }, err => {
+//         console.log(err, 'fail');
+//     });
+// }
 
-module promise31_async1 {
-    console.log('---------- async1 ----------')
-    const p = new Promise_Async<string>((resolve, reject) => {
-        setTimeout(() => {
-            /* 1 */
-            // resolve('成功');
+// module promise31_async1 {
+//     const p = new Promise_Async<string>((resolve, reject) => {
+//         setTimeout(() => {
+//             /* 1 */
+//             // resolve('成功');
 
-            /* 2 */
-            // reject('失败');
-        }, 1000);
-    });
-    p.then(val => {
-        console.log(val, 'success');
-    }, err => {
-        console.log(err, 'fail');
-    });
-    p.then(val => {
-        console.log(val, 'success');
-    }, err => {
-        console.log(err, 'fail');
-    });
-    p.then(val => {
-        console.log(val, 'success');
-    }, err => {
-        console.log(err, 'fail');
-    });
-}
+//             /* 2 */
+//             // reject('失败');
+//         }, 1000);
+//     });
+//     p.then(val => {
+//         console.log(val, 'success');
+//     }, err => {
+//         console.log(err, 'fail');
+//     });
+//     p.then(val => {
+//         console.log(val, 'success');
+//     }, err => {
+//         console.log(err, 'fail');
+//     });
+//     p.then(val => {
+//         console.log(val, 'success');
+//     }, err => {
+//         console.log(err, 'fail');
+//     });
+// }
 
-module promise31_async2 {
-    console.log('---------- async2 ----------');
-    read(_path('../name.txt')).then(data => {
-        /* 1 */
-        // console.log(data)
-        read(_path(data))
+// module promise31_async2 {
+//     read(_path('../name.txt')).then(data => {
+//         return read(_path(data))
+//     }).then(data => {
+//         console.log(data);
+//     })
 
-        /* 2 */
-    }).then(data => {
+
+//     function read(url: string) {
+//         return new Promise_Async<string>((resolve, reject) => {
+//             fs.readFile(url, 'utf8', (err, data) => {
+//                 if (err) return reject(err);
+//                 resolve(data);
+//             });
+//         });
+//     }
+// }
+
+// module promise31_async3 {
+//     const p = new Promise_Async<number>((resolve, reject) => {
+//         resolve(123)
+//     });
+
+//     const p2 = p.then(data => {
+//         throw new Error('123');
+//     }, err => {
+//         return err + 400;
+//     });
+//     p2.then(data => {
+//         console.log(data, '****');
+//     }, err => {
+//         console.log(err, 'error----');
+//     })
+// }
+
+module promise31_async4 {
+    const p = new Promise_Async<number>((resolve, reject) => {
+        resolve(123)
+    });
+
+    const p2: any = p.then(data => {
+        return p2;
+    });
+    p2.then(null, (data: any) => {
         console.log(data);
-    })
-
-
-    function read(url: string) {
-        return new Promise_Async<string>((resolve, reject) => {
-            fs.readFile(url, 'utf8', (err, data) => {
-                if (err) return reject(err);
-                resolve(data);
-            });
-        });
-    }
+    });
 }
-
-console.log('你好')
--- 3.promise/2.implement.ts
+++ 3.promise/2.implement.ts
@@ -2,26 +2,31 @@
  * @file promise实现
  */
 export namespace _ {
-    export type Resolved<T, TResult> = ((value: T) => TResult | PromiseLike<TResult>) | undefined | null;
-    export type Rejected<TResult> = ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null;
+    export type Status = 'pending' | 'fulfilled' | 'rejected';
+
+    export type InternalResolve<T> = (value: T | PromiseLike<T>) => void;
+    export type InternalReject = (reason?: any) => void;
+
+    export type FulfillmentHandler<T, TResult> = ((value: T) => TResult | PromiseLike<TResult>) | undefined | null;
+    export type RejectionHandler<TResult> = ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null;
 
     export interface PromiseLike<T> {
         then<TResult1 = T, TResult2 = never>(
-            onfulfilled?: Resolved<T, TResult1>,
-            onrejected?: Rejected<TResult2>
+            onfulfilled?: FulfillmentHandler<T, TResult1>,
+            onrejected?: RejectionHandler<TResult2>
         ): PromiseLike<TResult1 | TResult2>;
     }
 
 
-    export type Executor<T> = (resolve: (value: T | PromiseLike<T>) => void, reject: (reason?: any) => void) => void;
+    export type Executor<T> = (resolve: InternalResolve<T>, reject: InternalReject) => void;
 
     export interface Promise<T> {
         then<TResult1 = T, TResult2 = never>(
-            onfulfilled?: Resolved<T, TResult1>,
-            onrejected?: Rejected<TResult2>
+            onfulfilled?: FulfillmentHandler<T, TResult1>,
+            onrejected?: RejectionHandler<TResult2>
         ): Promise<TResult1 | TResult2>;
 
-        // catch<TResult = never>(onrejected?: Rejected<T>): Promise<T | TResult>;
+        // catch<TResult = never>(onrejected?: RejectionHandler<T>): Promise<T | TResult>;
     }
 
     export interface PromiseConstructor {
@@ -33,7 +38,7 @@ export namespace _ {
 
 // 解决同步状态更新
 export class Promise_Sync<T> /* implements _.Promise<T> */ {
-    status: string = 'pending';
+    status: _.Status = 'pending';
     value: any = undefined;
     reason: any = undefined;
     constructor(executor: _.Executor<T>) {
@@ -59,8 +64,8 @@ export class Promise_Sync<T> /* implements _.Promise<T> */ {
 
 
     then<TResult1 = T, TResult2 = never>(
-        onfulfilled?: _.Resolved<T, TResult1>,
-        onrejected?: _.Rejected<TResult2>
+        onfulfilled?: _.FulfillmentHandler<T, TResult1>,
+        onrejected?: _.RejectionHandler<TResult2>
     ) {
         if (this.status === 'fulfilled') {
             typeof onfulfilled === 'function' && onfulfilled(this.value);
@@ -73,17 +78,17 @@ export class Promise_Sync<T> /* implements _.Promise<T> */ {
 
 // 解决异步状态更新
 export class Promise_Async<T> /* implements _.Promise<T> */ {
-    private status: string = 'pending';
+    private status: _.Status = 'pending';
     private value: any = undefined;
     private reason: any = undefined;
 
     private onResolvedCallbacks: Function[] = [];
     private onRejectedCallbacks: Function[] = [];
 
-    private OwnConstructor: _.PromiseConstructor;
+    // private OwnConstructor: _.PromiseConstructor;
 
     constructor(executor: _.Executor<T>) {
-        this.OwnConstructor = new.target;
+        // this.OwnConstructor = new.target;
 
         const resolve = (value: T | _.PromiseLike<T>) => {
             if (this.status === 'pending') {
@@ -103,33 +108,99 @@ export class Promise_Async<T> /* implements _.Promise<T> */ {
         try {
             executor(resolve, reject);
         } catch (error) {
+            console.log(error);
             reject(error);
         }
     }
 
-
     then<TResult1 = T, TResult2 = never>(
-        onfulfilled?: _.Resolved<T, TResult1>,
-        onrejected?: _.Rejected<TResult2>
-    ) {
-        const promise2 = new this.OwnConstructor((resolve, reject) => {
+        onfulfilled?: _.FulfillmentHandler<T, TResult1>,
+        onrejected?: _.RejectionHandler<TResult2>
+    ): _.Promise<TResult1 | TResult2> {
+
+        const PConstructor: _.PromiseConstructor = (Object.getPrototypeOf(this).constructor)
+        const promise2= new PConstructor<TResult1 | TResult2>((resolve, reject) => {
+
+            let _onfulfilled = typeof onfulfilled === 'function' ? onfulfilled : (value: any) => value;
+            let _onrejected = typeof onrejected === 'function' ? onrejected : (reason: any) => reason;
+
             if (this.status === 'fulfilled') {
-                typeof onfulfilled === 'function' && onfulfilled(this.value);
+                setTimeout(() => {
+                    try {
+                        let x = _onfulfilled(this.value);
+                        this._resolvePromise(promise2, x, resolve, reject);
+                    } catch (error) {
+                        reject(error);
+                    }
+                });
             }
             if (this.status === 'rejected') {
-                typeof onrejected === 'function' &&  onrejected(this.reason);
+                setTimeout(() => {
+                    try {
+                        let x = _onrejected(this.reason);
+                        this._resolvePromise(promise2, x, resolve, reject);
+                    } catch (error) {
+                        reject(error);
+                    }
+                });
             }
             if (this.status === 'pending') {
                 this.onResolvedCallbacks.push(() => {
-                    typeof onfulfilled === 'function' && onfulfilled(this.value);
+                    setTimeout(() => {
+                        try {
+                            let x = _onfulfilled(this.value);
+                            this._resolvePromise(promise2, x, resolve, reject);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    })
                 });
                 this.onRejectedCallbacks.push(() => {
-                    typeof onrejected === 'function' && onrejected(this.reason);
+                    setTimeout(() => {
+                        try {
+                            let x = _onrejected(this.reason);
+                            this._resolvePromise(promise2, x, resolve, reject);
+                        } catch (error) {
+                            reject(error);
+                        }
+                    })
                 });
             }
         });
 
         return promise2;
+    }
+
+    private _resolvePromise<T>(
+        promise2: _.Promise<T>,
+        x: any,
+        resolve: _.InternalResolve<T>,
+        reject: _.InternalReject
+    ): void {
+        if (promise2 === x) {
+            return reject(new TypeError('循环引用'));
+        }
+
+        if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+            try {
+                const then = x.then;
+                if (typeof then === 'function') {
+                    then.call(x, (x: T) => {
+                        resolve(x);
+                    }, (y: any) => {
+                        reject(y);
+                    });
+                    // x.then() // 有可能第一次取值报错，第二次取值不报错
+                } else {
+                    resolve(x);
+                }
+            } catch (error) {
+                reject(error);
+            }
+
+        } else {
+            resolve(x);
+        }
     }
 }
 
